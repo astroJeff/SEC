8e20d0b215eac414da526f6108f3eb6465fae44a
Aaron Dotter <aaron.dotter@gmail.com>
2022-05-18 10:49:24 -0400

    fixing a few bugs in opacity

--- src/opacity.py
+++ src/opacity.py
@@ -1,4 +1,5 @@
-def calc_opacity(T, rho, x_mass, zion, aion, ionmax, pep, xne, eta):
+def calc_opacity(T, rho, xmass, zion, aion, ionmax, pep_in, xne_in, eta):
+    import numpy as np
     """ Calculate an approximate opacity.
 
     Parameters
@@ -23,12 +24,10 @@ def calc_opacity(T, rho, x_mass, zion, aion, ionmax, pep, xne, eta):
     sigma : total conductivity
     """
 
-
-
     # constants
     con2 = 1.07726359439811217e-7
     zbound = 0.1
-    t7peek = 1.0d20
+    t7peek = 1.0e20
     avo = 6.0221367e23
     meff = 1.194648642401440e-10
     weid = 6.884326138694269e-5
@@ -37,12 +36,21 @@ def calc_opacity(T, rho, x_mass, zion, aion, ionmax, pep, xne, eta):
     rt3 = 1.7320508075688772
     c = 2.99792458e10
     ssol = 5.67050407222e-5
-    asol = 4.0*ssol/c,
-    k2c = 4.0/3.0*asol*c
+    asol = 4.0*ssol/c
+    k2c = (4.0/3.0)*asol*c
 
+    tiny = 1.0e-20
+
+
+
+    xne = xne_in + tiny
+    pep = pep_in + tiny
+
+
+    
     # Switch for the Iben & Christy regimes
-    t6_switch1 = 1
-    t6_switch2 = 1.5
+    T6_switch1 = 1
+    T6_switch2 = 1.5
 
     # Initialize composition variables
     w = np.zeros(6)
@@ -74,56 +82,56 @@ def calc_opacity(T, rho, x_mass, zion, aion, ionmax, pep, xne, eta):
         xkap = 1 + xkc * (1 + xkc/24.55)
         xkb = 3.86 + 0.252*np.sqrt(xmu) + 0.018*xmu
         xka = 3.437 * (1.25 + 0.488*np.sqrt(xmu) + 0.092*xmu)
-        dbar = np.exp(-xka + xkb*np.log(t6))
-        oiben1 = xkap * (den/dbar)**0.67
+        dbar = np.exp(-xka + xkb*np.log(T6))
+        oiben1 = xkap * (rho/dbar)**0.67
 
-    if (xh<1.0e-5 or t6>t6_switch1) and (xh>=1.0e5 or xz<=zbound):
-
-        if t6>t6_switch1:
-            d0log = -(3.868 + 0.806*xh) + 1.8*np.log(t6)
+    if (xh<1.0e-5 or T6>T6_switch1) and (xh>=1.0e5 or xz<=zbound):
+        
+        if T6>T6_switch1:
+            d0log = -(3.868 + 0.806*xh) + 1.8*np.log(T6)
         else:
-            d0log = -(3.868 + 0.806*xh) + (3.42 - 0.52*xh)*np.log(t6)
+            d0log = -(3.868 + 0.806*xh) + (3.42 - 0.52*xh)*np.log(T6)
 
-       xka1 = 2.809 * np.exp(-(1.74 - 0.755*xh) * (np.log10(t6) - 0.22 + 0.1375*xh)**2)
-       xkw  = 4.05 * np.exp(-(0.306 - 0.04125*xh) * (np.log10(t6) - 0.18 + 0.1625*xh)**2)
-       xkaz = 50.0*xz*xka1 * np.exp(-0.5206*((np.log(rho)-d0log)/xkw)**2)
-       dbar2log = -(4.283 + 0.7196*xh) + 3.86*np.log(t6)
-       dbar1log = -5.296 + 4.833*np.log(t6)
-       if (dbar2log < dbar1log): dbar1log = dbar2log
-       oiben2   = (rho/np.exp(dbar1log))**(0.67) * np.exp(xkaz)
+        xka1 = 2.809 * np.exp(-(1.74 - 0.755*xh) * (np.log10(T6) - 0.22 + 0.1375*xh)**2)
+        xkw  = 4.05 * np.exp(-(0.306 - 0.04125*xh) * (np.log10(T6) - 0.18 + 0.1625*xh)**2)
+        xkaz = 50.0*xz*xka1 * np.exp(-0.5206*((np.log(rho)-d0log)/xkw)**2)
+        dbar2log = -(4.283 + 0.7196*xh) + 3.86*np.log(T6)
+        dbar1log = -5.296 + 4.833*np.log(T6)
+        if (dbar2log < dbar1log): dbar1log = dbar2log
+        oiben2   = (rho/np.exp(dbar1log))**(0.67) * np.exp(xkaz)
 
 
     # From Christy apj 144 108 1966
-    if (T6 < t6_switch2) and (xh >= 1e-5):
-       t4    = temp / 1.0e4
-       t4r   = np.sqrt(t4)
-       t44   = t4**4
-       t45   = t44 * t4
-       t46   = t45 * t4
-       ck1   = 2.0e6/t44 + 2.1*t46
-       ck3   = 4.0e-3/t44 + 2.0e-4/rho**(0.25)
-       ck2   = 4.5*t46 + 1.0/(t4*ck3)
-       ck4   = 1.4e3*t4 + t46
-       ck5   = 1.0e6 + 0.1*t46
-       ck6   = 20.0*t4 + 5.0*t44 + t45
-       xkcx  = xh*(t4r/ck1 + 1.0/ck2)
-       xkcy  = xhe*(1.0/ck4 + 1.5/ck5)
-       xkcz  = xz*(t4r/ck6)
-       ochrs = pep * (xkcx + xkcy + xkcz)
+    if (T6 < T6_switch2) and (xh >= 1e-5):
+        t4    = T / 1.0e4
+        t4r   = np.sqrt(t4)
+        t44   = t4**4
+        t45   = t44 * t4
+        t46   = t45 * t4
+        ck1   = 2.0e6/t44 + 2.1*t46
+        ck3   = 4.0e-3/t44 + 2.0e-4/rho**(0.25)
+        ck2   = 4.5*t46 + 1.0/(t4*ck3)
+        ck4   = 1.4e3*t4 + t46
+        ck5   = 1.0e6 + 0.1*t46
+        ck6   = 20.0*t4 + 5.0*t44 + t45
+        xkcx  = xh*(t4r/ck1 + 1.0/ck2)
+        xkcy  = xhe*(1.0/ck4 + 1.5/ck5)
+        xkcz  = xz*(t4r/ck6)
+        ochrs = pep * (xkcx + xkcy + xkcz)
 
 
     # Opacity in the presence of Hydrogen
     if xh >= 1e-5:
-        if T6 < t6_switch1:
+        if T6 < T6_switch1:
             orad = ochrs
-        elif T6 <= t6_switch2:
-            zdum  = 1.0/(t6_switch1 - t6_switch2)
-            xdum  = (T6 - t6_switch2)*zdum
-            ydum  = (T6 - t6_switch1)*zdum
+        elif T6 <= T6_switch2:
+            zdum  = 1.0/(T6_switch1 - T6_switch2)
+            xdum  = (T6 - T6_switch2)*zdum
+            ydum  = (T6 - T6_switch1)*zdum
             orad  = ochrs*xdum + oiben2*ydum
         else:
             orad = oiben2
-    # Opacity in the absence of Hydrogen
+            # Opacity in the absence of Hydrogen
     else:
        if (xz > zbound):
            orad = oiben1
@@ -132,14 +140,14 @@ def calc_opacity(T, rho, x_mass, zion, aion, ionmax, pep, xne, eta):
 
 
     # Add in Compton scattering opacity, Weaver et al. apj 1978 225 1021
-    th = np.min([511.0, temp * 8.617e-8])
+    th = np.min([511.0, T * 8.617e-8])
     fact = 1.0 + 2.75e-2*th - 4.88e-5*th*th
     facetax = 1.0e100
     if eta<500.0:
         facetax = np.exp(0.522*eta - 1.563)
-    faceta = 1.0 + facetax
-    ocompt = 6.65205e-25/(fact * faceta) * xne/den
-    orad = orad + ocompt
+        faceta = 1.0 + facetax
+        ocompt = 6.65205e-25/(fact * faceta) * xne/rho
+        orad = orad + ocompt
 
     # Cutoff radiative opacity when 4kt/hbar is less than the plasma frequency
     T_cut = con2 * np.sqrt(xne)
@@ -158,11 +166,11 @@ def calc_opacity(T, rho, x_mass, zion, aion, ionmax, pep, xne, eta):
 
     # Non-degenerate regimes, from Iben apj 196 525 1975
     log_rho = np.log10(rho)
-    drel = 2.4e-7 * zbar/abar * T * sqrt(T)
+    drel = 2.4e-7 * zbar/abar * T * np.sqrt(T)
     log_drel = np.log10(drel)
-    drelim = log_drel + 1.0d0
+    drelim = log_drel + 1.0
     if (log_rho < drelim):
-        zdel = xne/(avo*T6*np.sqrt(T6))
+        zdel = xne/(avo*T6*np.sqrt(T6)) 
         log_zdel = np.log10(zdel)
         eta0 = np.exp(-1.20322 + (2/3) * np.log(zdel))
         eta02 = eta0*eta0
@@ -171,9 +179,9 @@ def calc_opacity(T, rho, x_mass, zion, aion, ionmax, pep, xne, eta):
         if (log_zdel < 0.645):
             thpl = -7.5668 + np.log(zdel * (1.0 + 0.024417*zdel))
         else:
-            if log_zdel < 2.5):
+            if log_zdel < 2.5:
                 thpl = -7.58110 + np.log(zdel*(1.0 + 0.02804*zdel))
-                if (log_zdel > 2.0):
+                if log_zdel > 2.0:
                     thpla = thpl
                     thpl = -11.0742 + np.log(zdel**2 * (1.0 + 9.376/eta02))
                     thpl = 2.0*((2.5-log_zdel)*thpla + (log_zdel-2.0)*thpl)
@@ -190,7 +198,7 @@ def calc_opacity(T, rho, x_mass, zion, aion, ionmax, pep, xne, eta):
                 cfac2 = log_zdel - 1.5
                 pefac = np.exp(2.0 * (cfac1*pefacal + cfac2*pefacl))
         else:
-            pefac = 0.4 * eta0 + 1.64496d0/eta0
+            pefac = 0.4 * eta0 + 1.64496/eta0
 
         if zdel < 40.0:
             dnefac = 1.0 + zdel * (3.4838e-4 * zdel - 2.8966e-2)
@@ -209,13 +217,13 @@ def calc_opacity(T, rho, x_mass, zion, aion, ionmax, pep, xne, eta):
         if walf10 <= -3.0:
             thx = np.exp(2.413 - 0.124*walf)
         elif walf10 <= -1.0:
-            thx = exp(0.299 - walf*(0.745 + 0.0456*walf))
+            thx = np.exp(0.299 - walf*(0.745 + 0.0456*walf))
         else:
             thx = np.exp(0.426 - 0.558*walf)
 
         if walf10 <= -3.0:
             thy = np.exp(2.158 - 0.111*walf)
-        else if (walf10 <= 0.0):
+        elif (walf10 <= 0.0):
             thy = np.exp(0.553 - walf*(0.55 + 0.0299*walf))
         else:
             thy = np.exp(0.553 - 0.6*walf)
@@ -248,13 +256,13 @@ def calc_opacity(T, rho, x_mass, zion, aion, ionmax, pep, xne, eta):
         xrel = 1.009 * (zbar/abar * rho/1.0e6)**(1/3)
         beta2 = xrel**2/(1.0 + xrel**2)
         jy = (1 + 6/(5*xrel**2) + 2/(5*xrel**4)) * (yg**3/(3*(1+0.07414*yg)**3) * \
-            np.log((2.81 - 0.810*beta2 + yg)/yg) + np.pi**5/6 * yg**4/(13.91 + yg)**4 )
+                                                    np.log((2.81 - 0.810*beta2 + yg)/yg) + np.pi**5/6 * yg**4/(13.91 + yg)**4 )
         vee = 0.511 * T**2 * xmas/ymas**2 * np.sqrt(xmas/ymas) * jy
         cee = wfac/vee
 
         # total electron thermal conductivity and conversion to an opacity
         ov1 = cie * cee/(cee + cie)
-        ov = k2c/(ov1*rho) * temp**3
+        ov = k2c/(ov1*rho) * T**3
 
     # Blend the opacities in the intermediate region
     if log_rho <= log_drel:
